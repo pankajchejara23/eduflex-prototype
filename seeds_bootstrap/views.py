694fba9e0c4a7bae4fb5434e3299ec830eea12ff
Pankaj chejara <pankajchejara23@gmail.com>
2024-09-18 15:12:19 +0300

    update views

--- seeds_bootstrap/views.py
+++ seeds_bootstrap/views.py
@@ -6,15 +6,12 @@ from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.admin.views.decorators import staff_member_required
 from django.forms.models import model_to_dict
-from register import views as rv
-from register.forms import RegisterForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils import translation
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
-from register import forms as reg_forms
 from django.contrib.sessions.models import Session
 import pandas as pd
 import pprint as pp
